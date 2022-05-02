7c98e308d413b9e344b93805e8ccdfd57d535ef1
Beatriz Pietraroia <bia_pietraroia@hotmail.com>
2022-05-01 22:33:53 -0300

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const server = express();
+const jsonParse = bodyParser.json();
 
 const usuarios = [
     { id: 1, nome: "Diego", sobrenome: "Campos"}, 
     { id: 2, nome: "Ana", sobrenome: "Souza"}
 ];
 
-server.get('/', (req, res) =>{
+server.get('/usuarios', (req, res) =>{
     res.send(usuarios)
 });
 
@@ -22,6 +24,19 @@ server.get('/:id', (req, res) =>{
     const aux = usuarios.find((usuario) => usuario.id == id)
     res.send(aux);
 });
+
+server.post('/usuarios', jsonParse, (req, res) =>{
+    console.log(req.body)
+    const newUser = req.body
+    usuarios.push(newUser)
+    res.send(usuarios);
+});
+
+server.delete('/usuarios/:index', (req, res)=>{
+    const {index} = req.params
+    usuarios.splice(index,1)
+    res.send(usuarios);
+})
 
 console.log("O servidor ta rodando")
 
